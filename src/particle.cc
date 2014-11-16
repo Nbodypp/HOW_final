3c84f207287ff7f795ea18d92a3b30e94c413aa9
Semyeong Oh <semyeong.oh@gmail.com>
2014-11-15 23:05:33 -0500

    setup directory structure

---
+++ src/particle.cc
@@ -0,0 +1,12 @@
+#include "particle.h"
+
+Particle :: Particle(double mass, double radius, int dimen)
+  : mass_(mass),
+    radius_(radius),
+    dimen_(dimen)
+{}
+
+
+Orbit_Object::~Orbit_Object()
+{}
+
