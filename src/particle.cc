15a4c78e7dbdf572dc22f07751b3e415a8aaaf66
Semyeong Oh <semyeong.oh@gmail.com>
2014-11-23 16:47:32 -0500

    Implement Particle class

--- src/particle.cc
+++ src/particle.cc
@@ -1,19 +1,22 @@
 /**
- * \file
- *
- * \brief Particle class
- *
- * Longer description goes here
+ * @file
  */
 #include "particle.h"
+#include <stdio.h>
 
-Particle::Particle(double mass, double radius, int dimen)
-  : mass_(mass),
-    radius_(radius),
-    dimen_(dimen)
+Particle::Particle(double mass, double radius)
+ : mass(mass),
+   radius(radius)
 {}
 
 
 Particle::~Particle()
 {}
+
+
+void Particle::print()
+{
+  printf("%15.8f %15.8f %15.8f %15.8f %15.8f\n",
+         this->mass, this->radius, this->x, this->y, this->z);
+}
 
--- src/particle.h
+++ src/particle.h
@@ -1,27 +1,49 @@
 /**
- * \file
+ * @file
  *
- * \brief Particle class
- *
+ * @brief Particle class
  * Longer description goes here
  */
+
 #ifndef PARTICLE_H_
 #define PARTICLE_H_
 
 /**
- * \brief Particle class for each body
+ * @brief Particle class for each body.
  */
 class Particle {
  public:
-  Particle(double mass, double radius, int dimen);
+  /**
+   * Constructor
+   * @param mass mass of the particle
+   * @param radius radius of the particle
+   */
+  Particle(double mass, double radius);
+
+  /**
+   * Destructor
+   */
   ~Particle();
-  double r[dimen];
-  double v[dimen];
+
+  double mass;  /**< mass of the particle */
+  double radius;  /**< radius of the particle */
+
+  double x;  /**< position x */
+  double y;  /**< position y */
+  double z;  /**< position z */
+
+  double vx;  /**< velocity x */
+  double vy;  /**< velocity y */
+  double vz;  /**< velocity z */
+
+  double ax;  /**< acceleration x */
+  double ay;  /**< acceleration y */
+  double az;  /**< acceleration z */
+
+  void print();
 
  private:
-  const double mass_;
-  const double radius_;
-  const int    dimen_;
+  static const int dimen_ = 3;
 };
 
 #endif  // PARTICLE_H_
