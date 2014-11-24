002df1252064bb12ad7b09087d2debfa31c5709e
Semyeong Oh <semyeong.oh@gmail.com>
2014-11-23 20:11:12 -0500

    initalize position, velocity, and acceleration

--- src/particle.cc
+++ src/particle.cc
@@ -6,8 +6,12 @@
 
 Particle::Particle(double mass, double radius)
  : mass(mass),
-   radius(radius)
-{}
+   radius(radius),
+   x(0), y(0), z(0),
+   vx(0), vy(0), vz(0),
+   ax(0), ay(0), az(0)
+{
+}
 
 
 Particle::~Particle()
