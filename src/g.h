e4aab5cf0d42247be5aabe844cedc4d2c2d38599
Joshua <joshuawallace800@gmail.com>
2014-11-29 12:37:10 -0700

    acceleration.{h,cc} files written

---
+++ src/g.h
@@ -0,0 +1,11 @@
+/** \brief Definition of G, the gravitational constant
+
+    This header file defines G_ to be the value of the gravitational constant, in cgs units
+*/
+
+#ifndef G_H_
+#define G_H_
+
+#define G_ 6.67384e-8 /** G, the gravitational constant, in cgs units */
+
+#endif //G_H_
