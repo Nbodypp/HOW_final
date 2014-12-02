c896e262c16f769549fed20d64a3791bfd0916e5
coleholcomb <cholcomb@astro.princeton.edu>
2014-12-01 21:27:13 -0500

    Update and rename g.h to constants.h

--- src/constants.h
+++ src/constants.h
@@ -3,9 +3,9 @@
     This header file defines G_ to be the value of the gravitational constant, in cgs units
 */
 
-#ifndef G_H_
-#define G_H_
+#ifndef CONST_H_
+#define CONST_H_
 
 #define G_ 6.67384e-8 /** G, the gravitational constant, in cgs units */
 
-#endif //G_H_
+#endif //CONST_H_
