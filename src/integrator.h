fb7f57575e95ca0deb40a429baac66766874ca76
Cole <cholcomb@astro.princeton.edu>
2014-12-08 12:38:04 -0500

    added integrator.h

--- src/integrator.h
+++ src/integrator.h
@@ -1,1 +1,10 @@
-//
+#ifndef INTEGRATOR_H_
+#define INTEGRATOR_H_
+
+class Integrator {
+ public:
+  virtual ~Integrator() {}
+  virtual int Step(double t, double *x) = 0;
+};
+
+#endif  // INTEGRATOR_H_
