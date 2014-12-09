1c6d722bb8678f11b63a322a848b47616a84acd8
Semyeong Oh <semyeong.oh@gmail.com>
2014-12-08 23:48:14 -0500

    fix typo

--- src/euler.cc
+++ src/euler.cc
@@ -14,12 +14,12 @@ int Euler::step(double t, const Particles& particles) {
   force_.update_acceleration(particles);
   for (int i = 0; i < particles.size(); ++i)
   {
-    particles[i]->x = dt_ * particles[i]->vx;
-    particles[i]->y = dt_ * particles[i]->vy;
-    particles[i]->z = dt_ * particles[i]->vz;
-    particles[i]->vx = dt_ * particles[i]->ax;
-    particles[i]->vy = dt_ * particles[i]->ay;
-    particles[i]->vz = dt_ * particles[i]->az;
+    particles[i]->x += dt_ * particles[i]->vx;
+    particles[i]->y += dt_ * particles[i]->vy;
+    particles[i]->z += dt_ * particles[i]->vz;
+    particles[i]->vx += dt_ * particles[i]->ax;
+    particles[i]->vy += dt_ * particles[i]->ay;
+    particles[i]->vz += dt_ * particles[i]->az;
   }
   return 0;
 }
