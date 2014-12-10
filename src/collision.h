b655d38f0e9d969bb1bd6a326a313453e6143adf
Joshua Wallace <joshuajw@princeton.edu>
2014-12-10 11:41:36 -0500

    collision.cc, working on

---
+++ src/collision.h
@@ -0,0 +1,19 @@
+/**
+ *
+ *
+ * @brief Collision class, checks if two objects have collided
+ */
+
+#ifndef COLLISION_H_
+#define COLLISION_H_
+
+class Collision {
+ public:
+  Collision();
+  ~Collision();
+
+  int collision_check_all(const Particles& particles) const;
+};
+
+
+#endif  // COLLISION_H_
