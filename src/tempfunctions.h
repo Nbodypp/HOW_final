3ec3e0d5ca0a9ed28196093c3d450e79012b728f
Joshua Wallace <joshuajw@princeton.edu>
2014-12-17 17:22:40 -0500

    trying to incorporate extra-Newtonian forces

---
+++ src/tempfunctions.h
@@ -0,0 +1,10 @@
+#include "particle.h"
+
+#ifndef TEMP_FUNCTIONS_H_
+#define TEMP_FUNCTIONS_H_
+
+void f(Particles& particles, double**);
+
+void g(Particles& particles, double**);
+
+#endif  //TEMP_FUNCTIONS_H_
