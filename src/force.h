f8a09a36f32b81c2dc1a568dfc09d11d09968ce5
Joshua Wallace <joshuajw@princeton.edu>
2014-12-17 17:22:40 -0500

    trying to incorporate extra-Newtonian forces

--- src/force.h
+++ src/force.h
@@ -7,12 +7,24 @@
 #define FORCE_H_
 #include "particle.h"
 
+/*extra_force_func is a pointer to a function that takes Particles and 
+ *returns pointer to double.  It is the archetype for our extra-Newtonian
+ * functions*/
+typedef void (*extra_force_func)(Particles,double **); //Previously (double*)[3]
+
+/*Extra_Force_Func_Vec is a std::vector that stores the pointers to all 
+ *the extra-Newtonian force functions*/
+typedef std::vector<std::unique_ptr<extra_force_func>> Extra_Force_Func_Vec;
+
+
 class Force {
  public:
   Force();
   ~Force();
 
-  void update_acceleration(const Particles& particles) const;
+  void update_acceleration(const Particles& particles, const Extra_Force_Func_Vec &functions) const;
+
+  void extra_acceleration(const Particles& particles, const Extra_Force_Func_Vec &functions) const;
 };
 
 
