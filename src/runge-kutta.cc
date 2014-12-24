a723a518ffa69f97a42ebd634c5eebcc7d378fd1
Semyeong Oh <semyeong.oh@gmail.com>
2014-12-24 14:37:10 -0500

    add runge-kutta integrator. this one actually compiles

--- src/runge-kutta.cc
+++ src/runge-kutta.cc
@@ -1,126 +1,62 @@
-#include "rk4.h"
-#include "particles.h"
+#include "runge-kutta.h"
 
 /**
  * @param dt timestep
  * @param force Force model
  */
-RK4::RK4(double dt, const Force &force, const int np)
+RungeKutta4::RungeKutta4(const double dt, const Force &force)
   : dt_(dt),
-    force_(force) 
-    np_(np)	 {
-  k1_ = new double[np_][6];
-  k2_ = new double[np_][6];
-  k3_ = new double[np_][6];
-  k4_ = new double[np_][6];
+    force_(force) {
 }
 
-RK4::~RK4() {
-  delete k1_; 
-  delete k2_; 
-  delete k3_; 
-  delete k4_; 
-}
+RungeKutta4::~RungeKutta4()
+{}
 
-/** Step particles' position and velocity */
-int RK4::step(double t, const Particles& particles) {
-  Particles tempparticles; 
-  force_.update_acceleration(particles);
-  tempparticles = particles; //Not sure if this is valid
-
-  //Calculate k1 for all particles and dimensions
-  //Update tempparticles in preparation for k2
-  for (unsigned int i = 0; i < particles.size(); ++i)
+int RungeKutta4::step(double t, Particles &p) {
+  k1 = p;
+  force_.update_acceleration(k1);
+  k2 = k1;
+  for (int i = 0; i < k2.size(); ++i)
   {
-    k1_[i][1] = particles[i]->vx;
-    tempparticles[i]->x += 0.5*k1_[i][1];
-
-    k1_[i][2] = particles[i]->vy;
-    tempparticles[i]->y += 0.5*k1_[i][2];
-
-    k1_[i][3] = particles[i]->vz;
-    tempparticles[i]->z += 0.5*k1_[i][3];
-
-    k1_[i][4] = particles[i]->ax;
-    tempparticles[i]->vx += 0.5*k1_[i][4];
-
-    k1_[i][5] = particles[i]->ay;
-    tempparticles[i]->vy += 0.5*k1_[i][5];
-
-    k1_[i][6] = particles[i]->az;
-    tempparticles[i]->vz += 0.5*k1_[i][6];
+    k2[i].x  += k2[i].vx * dt_ * 0.5;
+    k2[i].y  += k2[i].vy * dt_ * 0.5;
+    k2[i].z  += k2[i].vz * dt_ * 0.5;
+    k2[i].vx += k2[i].ax * dt_ * 0.5;
+    k2[i].vy += k2[i].ay * dt_ * 0.5;
+    k2[i].vz += k2[i].az * dt_ * 0.5;
   }
-
-  //Calculate k2 for all particles and dimensions
-  //Update tempparticles in preparation for k3
-  force_.update_acceleration(tempparticles);
-  for (unsigned int i = 0; i < particles.size(); ++i)
+  force_.update_acceleration(k2);
+  k3 = k2;
+  for (int i = 0; i < k3.size(); ++i)
   {
-    k2_[i][1] = particles[i]->vx + 0.5*dt_*k1_[i][1];
-    tempparticles[i]->x = particles->x + 0.5*dt_*k2_[i][1];
-
-    k2_[i][2] = particles[i]->vy + 0.5*dt_*k1_[i][2];
-    tempparticles[i]->y = particles->y + 0.5*dt_*k2_[i][2];
-
-    k2_[i][3] = particles[i]->vz + 0.5*dt_*k1_[i][3];
-    tempparticles[i]->z = particles->z + 0.5*dt_*k2_[i][3];
-
-    k2_[i][4] = tempparticles[i]->ax;
-    tempparticles[i]->vx = particles->vx + 0.5*dt_*k2_[i][4];
-
-    k2_[i][5] = tempparticles[i]->ay;
-    tempparticles[i]->vy = particles->vy + 0.5*dt_*k2_[i][5];
-
-    k2_[i][6] = tempparticles[i]->az;
-    tempparticles[i]->vz = particles->vz + 0.5*dt_*k2_[i][6];
+    k3[i].x  += k3[i].vx * dt_ * 0.5;
+    k3[i].y  += k3[i].vy * dt_ * 0.5;
+    k3[i].z  += k3[i].vz * dt_ * 0.5;
+    k3[i].vx += k3[i].ax * dt_ * 0.5;
+    k3[i].vy += k3[i].ay * dt_ * 0.5;
+    k3[i].vz += k3[i].az * dt_ * 0.5;
   }
-
-  //Calculate k3 for all particles and dimensions
-  //Update tempparticles in preparation for k4
-  force_.update_acceleration(tempparticles);
-  for (unsigned int i = 0; i < particles.size(); ++i)
+  force_.update_acceleration(k3);
+  k4 = k3;
+  for (int i = 0; i < k4.size(); ++i)
   {
-    k3_[i][1] = particles[i]->vx + 0.5*dt_*k2_[i][1];
-    tempparticles[i]->x = particles->x + dt_*k3_[i][1];
-
-    k3_[i][2] = particles[i]->vy + 0.5*dt_*k2_[i][2];
-    tempparticles[i]->y = particles->y + dt_*k3_[i][2];
-
-    k3_[i][3] = particles[i]->vz + 0.5*dt_*k2_[i][3];
-    tempparticles[i]->z = particles->z + dt_*k3_[i][3];
-
-    k3_[i][4] = tempparticles[i]->ax;
-    tempparticles[i]->vx = particles->vx + dt_*k3_[i][4];
-
-    k3_[i][5] = tempparticles[i]->ay;
-    tempparticles[i]->vy = particles->vy + dt_*k3_[i][5];
-
-    k3_[i][6] = tempparticles[i]->az;
-    tempparticles[i]->vz = particles->vz + dt_*k3_[i][6];
+    k4[i].x  += k4[i].vx * dt_;
+    k4[i].y  += k4[i].vy * dt_;
+    k4[i].z  += k4[i].vz * dt_;
+    k4[i].vx += k4[i].ax * dt_;
+    k4[i].vy += k4[i].ay * dt_;
+    k4[i].vz += k4[i].az * dt_;
   }
-
-  //Calculate k4 for all particles and dimensions
-  force_.update_acceleration(tempparticles);
-  for (unsigned int i = 0; i < particles.size(); ++i)
+  force_.update_acceleration(k4);
+  // update finally
+  for (int i = 0; i < p.size(); ++i)
   {
-    k4_[i][1] = particles[i]->vx + dt_*k3_[i][1];
-    k4_[i][2] = particles[i]->vy + dt_*k3_[i][2];
-    k4_[i][3] = particles[i]->vz + dt_*k3_[i][3];
-    k4_[i][4] = tempparticles[i]->ax;
-    k4_[i][5] = tempparticles[i]->ay;
-    k4_[i][6] = tempparticles[i]->az;
+    p[i].x  += k1[i].vx*dt_/6. + (k2[i].vx + k3[i].vx)*dt_/3. + k4[i].vx*dt_/6.;
+    p[i].y  += k1[i].vy*dt_/6. + (k2[i].vy + k3[i].vy)*dt_/3. + k4[i].vy*dt_/6.;
+    p[i].z  += k1[i].vz*dt_/6. + (k2[i].vz + k3[i].vz)*dt_/3. + k4[i].vz*dt_/6.;
+    p[i].vx += k1[i].ax*dt_/6. + (k2[i].ax + k3[i].ax)*dt_/3. + k4[i].ax*dt_/6.;
+    p[i].vy += k1[i].ay*dt_/6. + (k2[i].ay + k3[i].ay)*dt_/3. + k4[i].ay*dt_/6.;
+    p[i].vz += k1[i].az*dt_/6. + (k2[i].az + k3[i].az)*dt_/3. + k4[i].az*dt_/6.;
   }
-
-  //Update all particles using k's
-  for (unsigned int i = 0; i < particles.size(); ++i)
-  {
-    particles[i]->x += dt_ * (k1_[i][1]/6 + (k2_[i][1]+k3_[i][1])/3 + k4_[i][1]/6);
-    particles[i]->y += dt_ * (k1_[i][2]/6 + (k2_[i][2]+k3_[i][2])/3 + k4_[i][2]/6)
-    particles[i]->z += dt_ * (k1_[i][3]/6 + (k2_[i][3]+k3_[i][3])/3 + k4_[i][3]/6)
-    particles[i]->vx += dt_ * (k1_[i][4]/6 + (k2_[i][4]+k3_[i][5])/3 + k4_[i][4]/6)
-    particles[i]->vy += dt_ * (k1_[i][5]/6 + (k2_[i][5]+k3_[i][5])/3 + k4_[i][5]/6)
-    particles[i]->vz += dt_ * (k1_[i][6]/6 + (k2_[i][6]+k3_[i][6])/3 + k4_[i][6]/6)
-  }
-
   return 0;
 }
--- src/runge-kutta.h
+++ src/runge-kutta.h
@@ -4,8 +4,8 @@
  * @brief Runge-Kutta 4th order integrator
  */
 
-#ifndef RK4_H_
-#define RK4_H_
+#ifndef RUNGE_KUTTA_H_
+#define RUNGE_KUTTA_H_
 
 #include "integrator.h"
 #include "force.h"
@@ -13,16 +13,15 @@
 /**
  * @brief Runge-Kutta 4th order integrator
  */
-class RK4 : public Integrator {
+class RungeKutta4 : public Integrator {
  public:
-  RK4(double dt,const Force &force);
-  ~RK4();
-  int step(double t, const Particles& particles);
+  RungeKutta4(const double dt, const Force &force);
+  ~RungeKutta4();
+  int step(double t, Particles &p);
  private:
   const double dt_;       // timestep
   const Force &force_;    // Force model
-  double *k1_, *k2_;	  // RK4 k's
-  double *k3_, *k4_;
+  Particles k1, k2, k3, k4;   // intermediate states
 };
 
-#endif  // RK4_H_
+#endif
