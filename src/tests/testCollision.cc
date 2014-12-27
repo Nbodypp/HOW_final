44c94a2df7a6396718634a5fe4d893c23308b36a
Semyeong Oh <semyeong.oh@gmail.com>
2014-12-26 23:03:53 -0500

    Speed up CATCH test compilation

--- src/tests/testCollision.cc
+++ src/tests/testCollision.cc
@@ -1,6 +1,4 @@
-#define CATCH_CONFIG_MAIN
 #include "catch.hpp"
-#include <stdio.h>
 
 #include "collision.h"
 #include "particle.h"
---
+++ src/tests/test_main.cc
@@ -0,0 +1,7 @@
+#define CATCH_CONFIG_MAIN
+#include "catch.hpp"
+
+// The purpose of this two line file is to speed up CATCH test compilation
+// as recommended at
+// https://github.com/philsquared/Catch/blob/master/docs/slow-compiles.md
+// Do not include `#define CATCH_CONFIG_MAIN` in individual test files.
