a0213363166831e632684bdb22eee290356be341
Semyeong Oh <semyeong.oh@gmail.com>
2015-01-05 13:58:35 -0500

    Simplify and fix formatting; put G only in test_main

--- src/tests/testCollision.cc
+++ src/tests/testCollision.cc
@@ -6,132 +6,105 @@
 
 TEST_CASE("Checking collision algorithm works", "[collision]")
 {
-
   //Create two identical particles
-  Particles particles ({
-    Particle (5.e5, 1.e3),
-    Particle (5.e5, 1.e3)
-    });
+  Particles particles (2, Particle (5.e5, 1.e3));
 
   REQUIRE(particles[0].mass == 5.e5);
   REQUIRE(particles[0].radius == 1.e3);
   REQUIRE(particles[1].mass == 5.e5);
   REQUIRE(particles[1].radius == 1.e3);
 
-
   SECTION("See if two particles placed on top of each "
-          "other are marked as colliding")
-    {
+          "other are marked as colliding") {
+    particles[0].x = 0.;
+    particles[0].y = 0.;      
+    particles[0].z = 0.;
 
-      particles[0].x = 0.;
-      particles[0].y = 0.;      
-      particles[0].z = 0.;
+    particles[1].x = 0.;
+    particles[1].y = 0.;      
+    particles[1].z = 0.;
 
-      particles[1].x = 0.;
-      particles[1].y = 0.;      
-      particles[1].z = 0.;
-
-      REQUIRE(collision_check_all(particles) == 1);
-
-    }
+    REQUIRE(collision_check_all(particles) == 1);
+  }
 
   SECTION("See if two particles somewhat off center "
-          "from  each other are marked as colliding")
-    {
+          "from  each other are marked as colliding") {
+    particles[0].x = 0.;
+    particles[0].y = 0.;      
+    particles[0].z = 0.;
 
-      particles[0].x = 0.;
-      particles[0].y = 0.;      
-      particles[0].z = 0.;
+    particles[1].x = 0.;
+    particles[1].y = 0.;      
+    particles[1].z = 999.;
 
-      particles[1].x = 0.;
-      particles[1].y = 0.;      
-      particles[1].z = 999.;
-
-
-      REQUIRE(collision_check_all(particles) == 1);
-
-    }
-
+    REQUIRE(collision_check_all(particles) == 1);
+  }
 
   SECTION("See if two particles significantly off center "
-          "from  each other are marked as colliding")
-    {
+          "from  each other are marked as colliding") {
+    particles[0].x = 0.;
+    particles[0].y = 0.;      
+    particles[0].z = 0.;
 
+    particles[1].x = 0.;
+    particles[1].y = 0.;      
+    particles[1].z = 1999.;
 
-      particles[0].x = 0.;
-      particles[0].y = 0.;      
-      particles[0].z = 0.;
-
-      particles[1].x = 0.;
-      particles[1].y = 0.;      
-      particles[1].z = 1999.;
-
-
-      REQUIRE(collision_check_all(particles) == 1);
-
-    }
+    REQUIRE(collision_check_all(particles) == 1);
+  }
 
   SECTION("See if two particles close to each other but not touching "
-          "are marked as NOT colliding")
-    {
+          "are marked as NOT colliding") {
+    particles[0].x = 0.;
+    particles[0].y = 0.;      
+    particles[0].z = 0.;
 
-      particles[0].x = 0.;
-      particles[0].y = 0.;      
-      particles[0].z = 0.;
+    particles[1].x = 0.;
+    particles[1].y = 0.;      
+    particles[1].z = 2001.;
 
-      particles[1].x = 0.;
-      particles[1].y = 0.;      
-      particles[1].z = 2001.;
-
-      REQUIRE(collision_check_all(particles) == 0);
-
-    }
+    REQUIRE(collision_check_all(particles) == 0);
+  }
 
   SECTION("See if two particles far away from each other "
-          "are marked as NOT colliding")
-    {
+          "are marked as NOT colliding") {
+    particles[0].x = 0.;
+    particles[0].y = 0.;      
+    particles[0].z = 0.;
 
-      particles[0].x = 0.;
-      particles[0].y = 0.;      
-      particles[0].z = 0.;
+    particles[1].x = 0.;
+    particles[1].y = 0.;      
+    particles[1].z = 1.e6;
 
-      particles[1].x = 0.;
-      particles[1].y = 0.;      
-      particles[1].z = 1.e6;
-
-
-      REQUIRE(collision_check_all(particles) == 0);
-
-    }
+    REQUIRE(collision_check_all(particles) == 0);
+  }
 
   SECTION("See if the function has correct capabilities for handling "
-          "multiple particles")
-    {
-      particles[0].x = 0.;
-      particles[0].y = 0.;      
-      particles[0].z = 0.;
+          "multiple particles") {
+    particles[0].x = 0.;
+    particles[0].y = 0.;      
+    particles[0].z = 0.;
 
-      particles[1].x = 0.995e5;
-      particles[1].y = 0.995e5;      
-      particles[1].z = 0.995e5;
+    particles[1].x = 0.995e5;
+    particles[1].y = 0.995e5;      
+    particles[1].z = 0.995e5;
 
-      particles.push_back(Particle(5.e3,1.e2));
-      particles.push_back(Particle(2.e5,1.e4));
+    particles.push_back(Particle(5.e3,1.e2));
+    particles.push_back(Particle(2.e5,1.e4));
 
-      REQUIRE(particles[2].mass == 5.e3);
-      REQUIRE(particles[2].radius == 1.e2);
-      REQUIRE(particles[3].mass == 2.e5);
-      REQUIRE(particles[3].radius == 1.e4);
+    REQUIRE(particles[2].mass == 5.e3);
+    REQUIRE(particles[2].radius == 1.e2);
+    REQUIRE(particles[3].mass == 2.e5);
+    REQUIRE(particles[3].radius == 1.e4);
 
-      particles[2].x = 1.e5;
-      particles[2].y = 1.e5;      
-      particles[2].z = 1.e5;
+    particles[2].x = 1.e5;
+    particles[2].y = 1.e5;      
+    particles[2].z = 1.e5;
 
-      particles[3].x = 1.e5;
-      particles[3].y = 1.e4;      
-      particles[3].z = 1.e10;
+    particles[3].x = 1.e5;
+    particles[3].y = 1.e4;      
+    particles[3].z = 1.e10;
 
-      REQUIRE(collision_check_all(particles) == 1);
-    }
-      
+    REQUIRE(collision_check_all(particles) == 1);
+  }   
 }
--- src/tests/testForce.cc
+++ src/tests/testForce.cc
@@ -4,26 +4,16 @@
 #include "force.h"
 #include <iostream>
 
-double G = 1.;
-
-
 
 //Test to make sure that force_direct (which uses
-// a normal Newtonian gravitation formula)
-// works as planned.
+//the Newtonian gravitation formula) works.
 TEST_CASE("Force direct","[forcedirect]")
 {
-  //Create two particles
-  Particles particles ({
-      Particle (2., 1.),
-        Particle (2., 1.)});
-
+  Particles particles (2, Particle (2., 1.));
   particles[0].x = 2.0;
   particles[1].x = -2.0;
 
-
   Force force;
-
   force.update_acceleration(particles);
 
   REQUIRE(particles[0].ax == -0.125);
@@ -34,10 +24,7 @@ TEST_CASE("Force direct","[forcedirect]")
   REQUIRE(particles[1].ay == 0.0);
   REQUIRE(particles[1].az == 0.0);
 
-  Particles particles2 ({
-      Particle (1., 1.),
-        Particle (1., 1.)});
-  
+  Particles particles2 (2, Particle (1., 1.));
   particles2[0].x = 3.0;
   particles2[1].y = 4.0;
   
@@ -51,38 +38,27 @@ TEST_CASE("Force direct","[forcedirect]")
   REQUIRE(particles2[1].ay == -0.032);
   REQUIRE(particles2[1].az == 0.0);
 
-  Particles particles3 ({
-      Particle (1., 1.),
-      Particle (1., 1.),
-      Particle (1., 1.),
-      Particle (1., 1.),
-        Particle (1., 1.)});
-
-  for(int i=0; i<5; ++i)
-    {
-      particles3[i].x = double(i);
-    }
+  Particles particles3 (5, Particle (1., 1.));
+  for(int i=0; i<5; ++i) {
+    particles3[i].x = double(i);
+  }
 
   force.update_acceleration(particles3);
 
-
-  for(int i=0; i<5; i++)
-    {
+  for(int i=0; i<5; i++) {
       REQUIRE(particles3[i].ay == 0.0);
       REQUIRE(particles3[i].az == 0.0);
-    }
+  }
   
   REQUIRE( (particles3[0].ax - 1.4236111111111 ) < 0.0000000001);
   REQUIRE( (particles3[1].ax - 0.361111111111111) < 0.0000000001);
   REQUIRE(particles3[2].ax == 0.0);
   REQUIRE( (particles3[3].ax - -0.361111111111111) < 0.0000000001);
   REQUIRE( (particles3[4].ax - -1.4236111111111) < 0.0000000001);
-  
 }
 
 void dragforce(Particles &p) {
-  for (int i = 0; i < p.size(); ++i)
-  {
+  for (int i = 0; i < p.size(); ++i) {
     p[i].ax -= p[i].vx;
     p[i].ay -= p[i].vy;
     p[i].az -= p[i].vz;
@@ -96,7 +72,6 @@ TEST_CASE("Test if add_force works", "[force]") {
   particles.back().vx = 3.;
 
   Force force;
-
   force.add_force(&dragforce);
   force.update_acceleration(particles);
   REQUIRE(particles[0].ax == -3);
@@ -107,42 +82,31 @@ TEST_CASE("Test if add_force works", "[force]") {
 //two extra forces
 
 //Here is an extra force
-void extra_force(Particles &particles)
-{
-  for (auto &p : particles)
-    {
-      p.ax = p.x;
-      p.ay = p.y;
-      p.az = p.z;
-    }
+void extra_force(Particles &particles) {
+  for (auto &p : particles) {
+    p.ax = p.x;
+    p.ay = p.y;
+    p.az = p.z;
+  }
 }
 
 //Here is a second extra force
-void extra_force2(Particles &particles)
-{
-  for (auto &p : particles)
-    {
-      p.az += 0.1;
-    }
+void extra_force2(Particles &particles) {
+  for (auto &p : particles) {
+    p.az += 0.1;
+  }
 }
 
 TEST_CASE("Extra forces","[forcedirect]")
 {
-  Particles particles({
-      Particle(0.,1.),
-      Particle(0.,1.),
-      Particle(0.,1.),
-      Particle(0.,1.)});
-
+  Particles particles(4, Particle(0.,1.));
   particles[0].x = 1.0;
   particles[1].y = 2.0;
   particles[2].z = -2.56;
 
   Force force;
-
   force.add_force(&extra_force);
   force.add_force(&extra_force2);
-  
   force.update_acceleration(particles);
 
   REQUIRE(particles[0].ax == 1.0);
